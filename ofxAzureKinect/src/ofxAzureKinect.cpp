5b3dcb3e2b1edfc9381fa3063a120e9368b8b3a8
Joseph Wells <joester3@gmail.com>
2019-07-19 17:24:20 -0700

    added update loop, utility functions

--- ofxAzureKinect/src/ofxAzureKinect.cpp
+++ ofxAzureKinect/src/ofxAzureKinect.cpp
@@ -9,6 +9,11 @@ ofxAzureKinect::ofxAzureKinect()
 
 	serial_number_length = 0;
 	serial_number = NULL;
+
+	bUseSynchronizedImages = true;
+
+	capture = NULL;
+	image = NULL;
 }
 
 ofxAzureKinect::~ofxAzureKinect()
@@ -21,11 +26,14 @@ bool ofxAzureKinect::init(k4a_image_format_t color_format, k4a_color_resolution_
 	k4a_depth_mode_t depth_mode, k4a_fps_t camera_fps)
 {
 	// Set the device configuration that will be passed to the device handle
-	k4a_device_configuration_t config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
-	config.camera_fps = camera_fps;
+	config = K4A_DEVICE_CONFIG_INIT_DISABLE_ALL;
 	config.color_format = color_format;
 	config.color_resolution = color_resolution;
 	config.depth_mode = depth_mode;
+	config.camera_fps = camera_fps;
+	config.synchronized_images_only = bUseSynchronizedImages;
+
+
 
 	ofLog(OF_LOG_NOTICE, "k4a device succesfully configured");
 
@@ -35,11 +43,22 @@ bool ofxAzureKinect::init(k4a_image_format_t color_format, k4a_color_resolution_
 bool ofxAzureKinect::open(int deviceIndex)
 {
 	// Open the first plugged in Kinect device
-	if (K4A_FAILED(k4a_device_open(K4A_DEVICE_DEFAULT, &device)))
+	if (K4A_FAILED(k4a_device_open(deviceIndex, &device)))
 	{
 		ofLog(OF_LOG_ERROR, "Failed to open k4a device!\n");
 		return false;
 	}
+
+	// Get device calibration to get width and height information
+	if (K4A_FAILED(k4a_device_get_calibration(device, config.depth_mode, config.color_resolution, &calibration)))
+	{
+		ofLog(OF_LOG_ERROR, "Failed to get calibration");
+		k4a_device_close(device);
+		return false;
+	}
+	width = calibration.color_camera_calibration.resolution_width;
+	height = calibration.color_camera_calibration.resolution_height;
+	ofLog(OF_LOG_NOTICE, "Color camera height: %d width: %d", height, width);
 
 	// Start the camera with the given configuration
 	if (K4A_FAILED(k4a_device_start_cameras(device, &config)))
@@ -50,7 +69,7 @@ bool ofxAzureKinect::open(int deviceIndex)
 	}
 
 	ofLog(OF_LOG_NOTICE, "k4a device succesfully opened");
-	return false;
+	return true;
 }
 
 bool ofxAzureKinect::close()
@@ -74,3 +93,37 @@ bool ofxAzureKinect::close()
 //
 //	return serial;
 //}
+
+void ofxAzureKinect::setSynchronizedImages(bool bUseSynchronizedImages)
+{
+	this->bUseSynchronizedImages = bUseSynchronizedImages;
+}
+
+float ofxAzureKinect::getHeight() const
+{
+	return height;
+}
+
+float ofxAzureKinect::getWidth() const
+{
+	return width;
+}
+
+void ofxAzureKinect::update()
+{
+	switch (k4a_device_get_capture(device, &capture, TIMEOUT_IN_MS))
+	{
+	case K4A_WAIT_RESULT_SUCCEEDED:
+		break;
+	case K4A_WAIT_RESULT_TIMEOUT:
+		ofLog(OF_LOG_WARNING, "Timed out waiting for a capture");
+		break;
+	case K4A_WAIT_RESULT_FAILED:
+		ofLog(OF_LOG_WARNING, "Failed to read a capture");
+		break;
+	}
+
+
+	// The capture must always be released when it is done being used
+	k4a_capture_release(capture);
+}
--- ofxAzureKinect/src/ofxAzureKinect.h
+++ ofxAzureKinect/src/ofxAzureKinect.h
@@ -12,7 +12,7 @@ public:
 	~ofxAzureKinect();
 
 	 bool init(k4a_image_format_t color_format = K4A_IMAGE_FORMAT_COLOR_BGRA32, 
-		 k4a_color_resolution_t color_resolution = K4A_COLOR_RESOLUTION_3072P, 
+		 k4a_color_resolution_t color_resolution = K4A_COLOR_RESOLUTION_1080P, 
 		 k4a_depth_mode_t depth_mode = K4A_DEPTH_MODE_NFOV_2X2BINNED, 
 		 k4a_fps_t camera_fps = K4A_FRAMES_PER_SECOND_30);
 	 
@@ -53,59 +53,17 @@ public:
 	 /// kinect models > 1414, so this will only work with the original xbox kinect
 	 string getSerial() const;
 
+	 void setSynchronizedImages(bool bUseSynchronizedImages = true);
+
 	 /// static kinect image size
-	 const static int width = 640;
-	 const static int height = 480;
 	 float getHeight() const;
 	 float getWidth() const;
 
-	 /// \section Static global kinect context functions
-
-		 /// print the device list
-	 static void listDevices();
-
-	 /// get the total number of devices
-	 static int numTotalDevices();
-
-	 /// get the number of available devices (not connected)
-	 static int numAvailableDevices();
-
-	 /// get the number of currently connected devices
-	 static int numConnectedDevices();
-
-	 /// is a device already connected?
-	 static bool isDeviceConnected(int id);
-	 static bool isDeviceConnected(string serial);
-
-	 /// get the id of the next available device,
-	 /// returns -1 if nothing found
-	 static int nextAvailableId();
-
-	 /// get the serial number of the next available device,
-	 /// returns an empty string "" if nothing found
-	 static string nextAvailableSerial();
-
-	 /// set the time to wait when not getting data before attempting to re-open device.
-	 static void setReconnectWaitTime(float waitTime);
 
 protected:
 
 	int deviceId;	///< -1 when not connected
 	string serial;	///< unique serial number, "" when not connected
-
-
-	bool bUseTexture;
-	ofTexture depthTex; ///< the depth texture
-	ofTexture videoTex; ///< the RGB texture
-	bool bGrabberInited;
-
-	ofPixels videoPixels;
-	ofPixels depthPixels;
-	ofShortPixels depthPixelsRaw;
-	ofFloatPixels distancePixels;
-
-	ofPoint rawAccel;
-	ofPoint mksAccel;
 
 	// for auto connect tries
 	float timeSinceOpen;
@@ -120,9 +78,26 @@ private:
 
 	k4a_device_t device;
 	k4a_device_configuration_t config;
+	k4a_calibration_t calibration;
 	
 	size_t serial_number_length;
 	char * serial_number;
 
+	bool bUseSynchronizedImages;
+
+	const int32_t TIMEOUT_IN_MS = 1000;
+	k4a_capture_t capture;
+	k4a_image_t image;
+
+	int height;
+	int width;
+
+	ofTexture depthTexture;
+	ofTexture colorTexture;
+	ofTexture irTexture;
+
+	ofPixels depthPixels;
+	ofPixels colorPixels;
+	ofPixels irPixels;
 
 };
